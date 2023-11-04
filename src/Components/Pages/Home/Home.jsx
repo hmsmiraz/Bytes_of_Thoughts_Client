c50ba1a6206585424c539799c9ec130846a41817
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-05 03:58:06 +0600

    Initial routing setup

---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='font-bold text-center text-5xl'>This is Home</h2>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import MainLayouts from "../Layouts/MainLayouts";
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayouts></MainLayouts>,
+      children:[
+        {
+            path: '/',
+            element: <Home></Home>,
+        },
+      ]
+    },
+  ]);
+
+  export default router;
