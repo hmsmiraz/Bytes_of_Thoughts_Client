9c3f4408898331642b05d727866e9fff2f263a30
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-07 15:09:43 +0600

    added featured blogs

--- src/Components/Pages/FeaturedBlogs/FeaturedBlogs.jsx
+++ src/Components/Pages/FeaturedBlogs/FeaturedBlogs.jsx
@@ -1,34 +1,58 @@
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import DataTable from "react-data-table-component";
 import { useLoaderData } from "react-router-dom";
 
 const FeaturedBlogs = () => {
   const blogs = useLoaderData();
   const [posts, setPosts] = useState(blogs);
 
-  const sortedData = posts.slice().sort((a, b) => a.longDescriptionWords - b.longDescriptionWords);
+  const sortedData = posts
+    .slice()
+    .sort((a, b) => a.longDescriptionWords - b.longDescriptionWords);
 
-  // function sortByLongDescriptionWordCount(posts) {
-  //   return posts.sort(
-  //     (a, b) => b.longDescription - a.longDescription
-  //   );
-  // }
-  // console.log(sortByLongDescriptionWordCount(posts));
-
-  // useEffect(() => {
-  //   const sortedPosts = sortByLongDescriptionWordCount(blogs);
-  //   setPosts(sortedPosts);
-  // }, [blogs]);
+  const dataWithSerialNumbers = sortedData.map((item, index) => ({
+    ...item,
+    serialNumber: index + 1,
+  }));
+  console.log(dataWithSerialNumbers);
+  const columns = [
+    {
+      name: "Serial Number",
+      selector: "serialNumber", // You should provide the appropriate field from your data
+      sortable: true,
+    },
+    {
+      name: "Blog Title",
+      selector: "title",
+      sortable: true,
+    },
+    {
+      name: "Blog Owner",
+      selector: "blogOwner", // Replace with the actual field name from your data
+    },
+    {
+      name: "Blog Owner Profile Picture",
+      cell: (row) => (
+        <img src={row.blogOwnerProfilePicture} alt={row.blogOwner} width="50" />
+      ),
+      sortable: false,
+    },
+    {
+      name: "Words in Long Description",
+      selector: "longDescriptionWords",
+      sortable: true,
+    },
+    // Add more columns as needed
+  ];
 
   return (
-    <ul>
-      {sortedData.map((post) => (
-        <li key={post._id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <DataTable
+        title="Blog Posts"
+        columns={columns}
+        data={dataWithSerialNumbers}
+      />
+    </div>
   );
 };
 
